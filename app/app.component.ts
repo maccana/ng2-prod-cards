7b8fc4ad499931182920fe0a67d991b23f53c99c
maccana <mikeymccann@gmail.com>
2016-12-26 16:44:04 -0500

    Added router-outlet

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,14 @@
 import { Component } from '@angular/core';
 
+import { ROUTER_DIRECTIVES } from '@angular/router';
+
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `
+        <h1>Hello {{ name }}</h1>
+        <!-- Routed views go here -->
+        <router-outlet></router-outlet>
+        `,
+     directives: [ROUTER_DIRECTIVES]
 })
 export class AppComponent  { name = 'Angular'; }
