0c12c6d892df52d13d8caa14ea0eb23309aefe0d
maccana <mikeymccann@gmail.com>
2016-12-26 16:56:43 -0500

    Changed template for templateUrl

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,12 +3,16 @@ import { Component } from '@angular/core';
 import { ROUTER_DIRECTIVES } from '@angular/router';
 
 @Component({
+  // selector: 'my-app',
+  // template: `
+  //       <h1>Hello {{ name }}</h1>
+  //       <!-- Routed views go here -->
+  //       <router-outlet></router-outlet>
+  //       `,
+  moduleId:module.id,
   selector: 'my-app',
-  template: `
-        <h1>Hello {{ name }}</h1>
-        <!-- Routed views go here -->
-        <router-outlet></router-outlet>
-        `,
+  // template: '<h1>My First Angular 2 App</h1><profile></profile>',
+  templateUrl: 'app.component.html',
      directives: [ROUTER_DIRECTIVES]
 })
 export class AppComponent  { name = 'Angular'; }
