1169d4d1d9e130f7233ed0d6fc4e93986959a34f
maccana <mikeymccann@gmail.com>
2016-12-27 02:37:24 -0500

    Imported navbar.component to app.module.ts

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,17 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent }  from './app.component';
 
+import { DashboardComponent }   from './dashboard.component';
+import { HomeComponent }   from './home.component';
+import { NavbarComponent }   from './navbar.component';
+
+
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  imports:      [ BrowserModule, AppRoutingModule ],
+  declarations: [ AppComponent, DashboardComponent, HomeComponent, NavbarComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
